c89b642ee9152d610ee89cac923d6062445b865f
Mukti Dirtama <muktidirtama@gmail.com>
2015-10-31 16:47:27 +0700

    mukti dirtama(1204105010040)

---
+++ 14-Prak-KoordinatGPS/KoordinatGPS.java
@@ -0,0 +1,31 @@
+
+public class KoordinatGPS
+{
+    public KoordinatGPS(){
+        this.lintang = new Double(0);
+        this.bujur = new Double(0);
+    }
+
+    
+public KoordinatGPS(double bujur, double lintang){
+    this.bujur = bujur;
+    this.lintang = lintang;
+}
+
+public KoordinatGPS(Double bujur, Double lintang){
+    this.bujur = bujur;
+    this.lintang = lintang;
+}
+
+public Double lintang(){
+    return lintang;
+}
+
+public Double bujur(){
+    return bujur;
+}
+
+private Double lintang;
+private Double bujur;
+
+}
